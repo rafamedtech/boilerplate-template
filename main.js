97354ab5d3766e7bf9f5e84d17ba5c6121ed5b07
Rafael <rafa.ravg@gmail.com>
2022-06-14 15:54:56 -0700

    boilerplate finished

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+const nav = document.querySelector('header');
+
+// Fixed Navbar transition
+window.addEventListener('scroll', () => {
+  if (window.scrollY > nav.offsetHeight + 50) {
+    nav.classList.add('nav-active');
+    nav.classList.remove('md:bg-transparent');
+  } else {
+    nav.classList.remove('nav-active');
+    nav.classList.add('md:bg-transparent');
+  }
+});
